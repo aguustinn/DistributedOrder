deeb5528496f74ac9dd7959e7abcf057d43a7e2d
agustinn-dev <agustinpenelas@gmail.com>
2026-05-24 01:09:17 -0300

    Ajeitando OrderStatus

--- DistributedOrders/src/NotificationService/Consumers/OrderStatusConsumer.cs
+++ DistributedOrders/src/NotificationService/Consumers/OrderStatusConsumer.cs
@@ -71,9 +71,9 @@ public class OrderStatusConsumer(
         return Task.CompletedTask;
     }
 
-    public override async ValueTask DisposeAsync()
+    public async ValueTask DisposeAsync()
     {
         if (_processor is not null) await _processor.DisposeAsync();
-        await base.DisposeAsync();
+        base.Dispose();
     }
 }
